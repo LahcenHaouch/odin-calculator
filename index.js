84d0f773cd908c10c76bcea0b82d3cf8ac4d9e14
LahcenHaouch <lahcen.haouch@protonmail.com>
2023-05-02 23:52:02 +0200

    Add event listener to each number btn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const numberElements = document.querySelectorAll('.btn-number');
+const numberElements = document.querySelectorAll(".btn-number");
+const calculatorDisplayElement = document.querySelector("#calculator-display");
+
+numberElements.forEach((element) =>
+  element.addEventListener("click", (event) => {
+    calculatorDisplayElement.textContent += event.target.textContent;
+  })
+);
