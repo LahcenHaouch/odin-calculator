e499a5d1831f9508913d807a426e665b6c0c27ee
LahcenHaouch <lahcen.haouch@protonmail.com>
2023-05-20 00:05:02 +0200

    Display what the user types

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
-const numberElements = document.querySelectorAll(".btn-number");
+const result = [];
+
+const numberElements = document.querySelectorAll(".num");
 const calculatorDisplayElement = document.querySelector("#calculator-display");
 
-numberElements.forEach((element) =>
-  element.addEventListener("click", (event) => {
-    calculatorDisplayElement.textContent += event.target.textContent;
+const operationsElements = document.querySelectorAll('.operation');
+
+numberElements.forEach(num => {
+  num.addEventListener('click', () => {
+    const lastElementIndex = result.length - 1;
+
+    if (lastElementIndex < 0) {
+      result.push(num.textContent);
+      displayResult();
+      return;
+    }
+
+    if (result[lastElementIndex]?.operation) {
+      result.push(num.textContent);
+    } else {
+      result[lastElementIndex] += num.textContent;
+    }
+    displayResult();
   })
-);
+})
+
+operationsElements.forEach(operation => {
+  operation.addEventListener('click', () => {
+    const lastElementIndex = result.length - 1;
+
+    if (lastElementIndex < 0) {
+      return;
+    }
+
+    const operationToAdd = { operation: operation.textContent };
+
+    if (result[lastElementIndex]?.operation) {
+      result[lastElementIndex] = operationToAdd;
+    } else {
+      result.push(operationToAdd);
+    }
+    displayResult();
+  })
+})
+
+
+function displayResult() {
+  calculatorDisplayElement.textContent = result.map(element => {
+    if (element.operation) {
+      return element.operation;
+    }
+    return element;
+  }).join('');
+}
